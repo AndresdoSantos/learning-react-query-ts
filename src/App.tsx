52665d9382ddd796d589bf8525a95912a6487013
andresdosantos <andres.dosantosbritoamaral@gmail.com>
2022-07-31 15:05:12 -0300

    feat(App.tsx): use useQuery

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
+
 import { User } from './components/User'
 import { UserDTO } from './DTOs/UserDTO'
 import { api } from './services/api'
@@ -6,27 +7,31 @@ import { api } from './services/api'
 import './styles/App.css'
 
 function App() {
-  const [userList, setUserList] = useState([] as UserDTO[])
-
-  async function getUsers() {
-    const { data } = await api.get<UserDTO[]>('users')
-
-    setUserList(data)
-  }
-
-  useEffect(() => {
-    getUsers()
-  }, [])
+  const { data, error, isLoading } = useQuery(['user-list'], () =>
+    api.get<UserDTO[]>('users'),
+  )
 
   return (
     <div className="container">
-      <h1>User List</h1>
+      {error ? (
+        <p>{JSON.stringify(error)}</p>
+      ) : (
+        <>
+          {isLoading ? (
+            <p>Loading</p>
+          ) : (
+            <>
+              <h1>User List</h1>
 
-      <ul className="list">
-        {userList.map((user) => (
-          <User {...user} />
-        ))}
-      </ul>
+              <ul className="list">
+                {data?.data.map((user) => (
+                  <User {...user} />
+                ))}
+              </ul>
+            </>
+          )}
+        </>
+      )}
     </div>
   )
 }
